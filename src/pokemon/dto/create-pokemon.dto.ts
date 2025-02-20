c4897479c156fce61b43a9fdd4e2738e47deee1e
Salomon <esteban.vivanco.progra@gmail.com>
2025-02-19 23:11:03 -0300

    feat: conexión con mongoose y post

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePokemonDto {}
+import { IsNumber, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @IsString()
+    @MinLength(3)
+    name: string;
+
+    @IsNumber()
+    @IsPositive()
+    no: number;
+
+
+}
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,20 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+
+    @Prop({
+        required: true,
+        unique: true
+    })
+    name: string;
+    @Prop({
+        required: true,
+        unique: true
+    })
+    no: number;
+
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
